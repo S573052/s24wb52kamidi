25ac0ad70a7758825adcc913fce5b5ff65c56c32
S573052 <S573052@nwmissouri.edu>
2024-03-27 11:18:34 -0500

    updated

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const booksRouter = require('./routes/books');
+const gridRouter = require('./routes/grid');
 
 var app = express();
 
@@ -23,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/books', booksRouter);
-
+app.use('/grid', gridRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
